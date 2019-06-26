f0677cd3d8b5d159e8e0fdb22e0d57a572f8d4bb
Ryan Flach <ryanflach@users.noreply.github.com>
2019-06-25 18:30:06 -0600

    Update dictionary app to work with hangman app

--- dictionary/lib/dictionary.ex
+++ dictionary/lib/dictionary.ex
@@ -1,10 +1,12 @@
 defmodule Dictionary do
   def random_word() do
-    Enum.random(word_list())
+    word_list()
+    |> Enum.random()
   end
 
   def word_list() do
-    "assets/words.txt"
+    "../assets/words.txt"
+    |> Path.expand(__DIR__)
     |> File.read!
     |> String.split(~r/\n/)
   end
