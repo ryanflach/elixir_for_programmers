76f2ed27b5440b2ea7158bbff0122717ad069b2d
Ryan Flach <ryanflach@users.noreply.github.com>
2019-06-29 12:30:31 -0600

    Add logic and specs to determine if a letter has already been guessed

--- hangman/lib/hangman/game.ex
+++ hangman/lib/hangman/game.ex
@@ -2,7 +2,8 @@ defmodule Hangman.Game do
   defstruct(
     turns_left: 7,
     game_state: :initializing,
-    letters: []
+    letters: [],
+    used: MapSet.new()
   )
 
   def new_game() do
@@ -12,6 +13,18 @@ defmodule Hangman.Game do
   def make_move(%__MODULE__{game_state: state} = game, _guess) when state in ~w(won lost)a,
     do: {game, tally(game)}
 
+  def make_move(%__MODULE__{} = game, guess) do
+    game = accept_move(game, guess, MapSet.member?(game.used, guess))
+    {game, tally(game)}
+  end
+
   defp tally(_guess), do: 123
 
+  defp accept_move(%__MODULE__{} = game, guess, _already_guessed = true) do
+    game |> Map.put(:game_state, :already_used)
+  end
+
+  defp accept_move(%__MODULE__{} = game, guess, _already_guessed) do
+    game |> Map.put(:used, MapSet.put(game.used, guess))
+  end
 end
