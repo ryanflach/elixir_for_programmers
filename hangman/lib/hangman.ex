636bd388027d92c407855f9e8274ceadfa081624
Ryan Flach <ryanflach@users.noreply.github.com>
2019-07-08 16:59:46 -0600

    Update Hangman API to utilize GenServer implementation

--- hangman/lib/hangman.ex
+++ hangman/lib/hangman.ex
@@ -1,6 +1,10 @@
 defmodule Hangman do
-  alias Hangman.Game
-  defdelegate new_game(), to: Game
-  defdelegate tally(game), to: Game
-  defdelegate make_move(game, guess), to: Game
+  alias Hangman.Server
+
+  def new_game(), do: Server.start_link()
+
+  def tally(game_pid), do: GenServer.call(game_pid, {:tally})
+
+  def make_move(game_pid, guess),
+    do: GenServer.call(game_pid, {:make_move, guess})
 end
--- hangman/lib/hangman/server.ex
+++ hangman/lib/hangman/server.ex
@@ -3,16 +3,14 @@ defmodule Hangman.Server do
 
   use GenServer
 
-  def start_link() do
-    GenServer.start_link(__MODULE__, nil)
-  end
+  def start_link(), do: GenServer.start_link(__MODULE__, nil)
 
-  def init(_) do
-    {:ok, Game.new_game()}
-  end
+  def init(_), do: {:ok, Game.new_game()}
 
   def handle_call({:make_move, guess}, _from, game) do
     {game, tally} = Game.make_move(game, guess)
     {:reply, tally, game}
   end
+
+  def handle_call({:tally}, _from, game), do: {:reply, Game.tally(game), game}
 end
