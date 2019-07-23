937e7a2b69be94c33d1e76e0142a8dcf7077b492
Ryan Flach <ryanflach@users.noreply.github.com>
2019-07-22 19:17:14 -0600

    Complete game play frontend logic and refactor to better honor SRP

--- gallows/lib/gallows_web/views/hangman_view.ex
+++ gallows/lib/gallows_web/views/hangman_view.ex
@@ -1,5 +1,14 @@
 defmodule GallowsWeb.HangmanView do
   use GallowsWeb, :view
 
+  import Gallows.Views.Helpers.GameStateHelper
+
+  def new_game_button(conn) do
+    button("New Game", to: Routes.hangman_path(conn, :create_game))
+  end
+
+  def game_over?(state) when state in ~w(won lost)a, do: true
+  def game_over?(_state), do: false
+
   def word_so_far(tally), do: tally.letters |> Enum.join(" ")
 end
