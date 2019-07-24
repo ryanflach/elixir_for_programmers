fe617e0d90ee9cb209e60b66792a1cc583211de9
Ryan Flach <ryanflach@users.noreply.github.com>
2019-07-24 16:25:31 -0600

    Add letters guessed to the game display

--- gallows/lib/gallows_web/views/hangman_view.ex
+++ gallows/lib/gallows_web/views/hangman_view.ex
@@ -12,6 +12,8 @@ defmodule GallowsWeb.HangmanView do
 
   def word_so_far(tally), do: tally.letters |> Enum.join(" ")
 
+  def letters_guessed(tally), do: tally.letters_guessed |> Enum.join(" ")
+
   def turn(remaining, target) when target >= remaining, do: "opacity: 1.0"
   def turn(_remaining, _target), do: "opacity: 0.1"
 end
